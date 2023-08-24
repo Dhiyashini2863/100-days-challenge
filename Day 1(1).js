2876bb0fd1266a677eecaf53758f3cc1f923b833
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-24 11:15:54 +0530

    Addition Of two Numbers By Default Input

---
+++ Day 1(1).js
@@ -0,0 +1,7 @@
+//Adiition Of two Numbers by Default Input
+const a=10;
+const b=20;
+// addition of two numbers
+const sum=a+b;
+// display the sum
+console.log('The sum of ' a + ' and ' + b + ' is: ' + sum);
