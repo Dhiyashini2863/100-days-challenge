d0702fab112fa365d45866353a7b99d493f6feac
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-24 11:22:26 +0530

    addition of two numbers by getting input from user

---
+++ Day 1(2).js
@@ -0,0 +1,9 @@
+//GETTING INPUT FROM USER
+const a = parseInt(prompt('Enter the first number '));
+const b = parseInt(prompt('Enter the second number '));
+
+//addition of two numbers
+const sum = a + b;
+
+// displaying the sum
+console.log(`The sum of ${a} and ${b} is ${sum}`);
