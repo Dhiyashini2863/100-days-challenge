adbe3f74a3b00b5d3daa22458a697a3c325f3042
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-24 18:40:20 +0530

    general mathematical functions

---
+++ Day1(3).js
@@ -0,0 +1,17 @@
+// take the input from the user
+const a = prompt('Enter the number: ');
+// square root
+const squareroot = Math.sqrt(a);
+console.log(`The square root of ${a} is ${squareroot}`);
+// cube root
+const cuberoot= Math.cbrt(a);
+console.log(`The cube root of ${a} is ${cuberoot}`);
+// sine theta
+const sine = Math.sinh(a);
+console.log(`The sine theta of ${a} is ${sine}`);
+// cos theta
+const cos =Math.cosh(a);
+console.log(`The cos theta of ${a} is ${cos}`);
+//tan theta
+const tan =Math.tanh(a);
+console.log(`The tan theta of ${a} is ${tan}`);
