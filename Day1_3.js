739e17b62b02c6d5ae3540ee54487edb8a16e31b
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-26 19:36:30 +0530

    Some mathematical functions

--- Day1_3.js
+++ Day1_3.js
@@ -12,6 +12,6 @@ console.log(`The sine theta of ${a} is ${sine}`);
 // cos theta
 const cos =Math.cosh(a);
 console.log(`The cos theta of ${a} is ${cos}`);
-//tan theta
+// tan theta
 const tan =Math.tanh(a);
 console.log(`The tan theta of ${a} is ${tan}`);
