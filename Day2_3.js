c24a650dd27e8bfd2a3caafd5b6bcc15cfdf1523
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-26 19:47:55 +0530

    Conversion Of Celsius To Fahrenheit

---
+++ Day2_3.js
@@ -0,0 +1,8 @@
+// getting celsius value from the user 
+const celsius = prompt("Enter a value in celsius: ");
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius = ${fahrenheit} degree fahrenheit.`);
