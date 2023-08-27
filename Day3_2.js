c034ca329c7dde8b6760962617b6d40fa73e7c13
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-27 10:32:45 +0530

    Random number between two numbers

---
+++ Day3_2.js
@@ -0,0 +1,9 @@
+//getting input from the user
+const low = parseInt(prompt("Enter a lowest number: "));
+const high = parseInt(prompt("Enter a highest number: "));
+
+// generating a random number
+const r = Math.floor(Math.random() * (high - low + 1)) + low;
+
+// display a random number
+console.log(`Random number between ${low} and ${high} is ${num}`);
