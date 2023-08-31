08342f02c7b8ef057033f61e9c456ffbc85e3ae4
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-08-31 11:09:59 +0530

    Checking the number type

---
+++ 07_number_type.js
@@ -0,0 +1,20 @@
+// getting input from the user
+const num = parseInt(prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (num > 0) 
+{
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (num == 0) 
+{
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else 
+{
+     console.log("The number is negative");
+}
