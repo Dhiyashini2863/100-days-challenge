263edc1cae0e8f4101752764584596d82735dfab
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-09-01 21:48:23 +0530

    Create 08_bitwise_op.js

---
+++ 08_bitwise_op.js
@@ -0,0 +1,18 @@
+
+// bitwise AND operator example
+result = a & b; 
+console.log(result); // 8 
+
+// bitwise OR operator example
+result = a | b; 
+console.log(result); // 29
+
+// bitwise XOR operator example
+result = a ^ b; 
+console.log(result); // 21
+
+// bitwise NOT operator example
+result = ~b;
+console.log(result); // -13
+
+
