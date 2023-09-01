967c0f0eb43d84952467dcd5c94bd3af773399fb
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-09-01 15:26:21 +0530

    Mathematical operators

---
+++ 08_operators.js
@@ -0,0 +1,20 @@
+// getting input from the users
+const x =  parseInt(prompt('Enter a number: '));
+const y =  parseInt(prompt('Enter another number: '));
+// addition
+console.log('x + y = ', x + y);
+
+// subtraction
+console.log('x - y = ', x - y);
+
+// multiplication
+console.log('x * y = ', x * y);
+
+// division
+console.log('x / y = ', x / y);
+
+// remainder
+console.log('x % y = ', x % y); 
+
+//exponentiation
+console.log('x ** y =', x ** y);
