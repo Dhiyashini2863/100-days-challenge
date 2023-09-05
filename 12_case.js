0e7238c0fb0d7fb8fd5ef9bff36f206cee8f1784
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-09-05 18:35:22 +0530

    Converting cases

---
+++ 12_case.js
@@ -0,0 +1,12 @@
+const message = "HELLO BHAI";
+
+// convert message to lowercase
+const lowerMessage = message.toLowerCase();
+console.log(lowerMessage);
+
+const message2 = "hello bhai";
+
+// convert message to uppercase
+const upperMessage = message2.toUpperCase();
+console.log(upperMessage);
+
