7700272da66a61a67ae14cb2ffe27f949c2b5ce8
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-09-05 18:31:45 +0530

    Replace string value

--- 12_str_replace.js
+++ 12_str_replace.js
@@ -1,3 +1,9 @@
 const message = "HELLO GITHUB"
+// replace all h with j
 let replace = message.replaceAll('H','J');
 console.log(replace);
+const message2 = "he is mine";
+
+// replace the first b with c
+let result = message2.replace('e', 'c');
+console.log(result);
