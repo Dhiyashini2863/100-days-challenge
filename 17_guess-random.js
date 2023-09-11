ae3cc45350f331e09fe909914efdfd3fbf4c7a6d
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-09-11 18:54:22 +0530

    Guessing random number

---
+++ 17_guess-random.js
@@ -0,0 +1,25 @@
+function guess() 
+{
+
+    // generating a random integer from 1 to 10
+    const random = Math.floor(Math.random() * 10) + 1;
+
+    // take input from the user
+    let number = parseInt(prompt('Guess a number from 1 to 10: '));
+
+    // take the input until the guess is correct
+    while(number !== random) 
+    {
+        number = parseInt(prompt('Guess a number from 1 to 10: '));
+    }
+
+    // check if the guess is correct
+    if(number == random) 
+    {
+        console.log('You guess is right.');
+    }
+
+  }
+
+// function call
+guess();
