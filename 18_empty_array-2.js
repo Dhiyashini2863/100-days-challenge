00ce6abb466748f2d7fe68d38f91a12f4ecc9df6
Dhiyashini U <133501522+Dhiyashini2863@users.noreply.github.com>
2023-09-12 22:20:21 +0530

    Empty an array

---
+++ 18_empty_array-2.js
@@ -0,0 +1,15 @@
+function emptyArray(arr) 
+{
+
+    // substituting new array
+    arr.splice(0, arr.length);
+    
+    return arr;
+}
+
+const array = [1, 2 ,3];
+console.log(array);
+
+// call the function
+const result = emptyArray(array);
+console.log(result);
